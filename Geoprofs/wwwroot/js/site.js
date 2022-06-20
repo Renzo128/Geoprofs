913458aec171db0733768b724afc5908e24c8bec
LorangeJuicey <60689696+LorangeJuicey@users.noreply.github.com>
2022-06-20 12:57:06 +0200

    final pop-up changes

--- Geoprofs/wwwroot/js/site.js
+++ Geoprofs/wwwroot/js/site.js
@@ -4,3 +4,14 @@
 function ShowPopUp(element_id) {
     $("#" + element_id).toggleClass("show");
 }
+
+$(".img-icon, #nieuwe-aanvraag, .form-shadow").click(function () {
+    $(".form-shadow, .create-request").toggleClass("hidden");
+});
+
+document.getElementById("AbsenceStart").addEventListener("change", function () {
+    console.log("test");
+    var firstdate = document.getElementById("AbsenceStart").value;
+    document.getElementById("AbsenceEnd").value = "";
+    document.getElementById("AbsenceEnd").setAttribute("min", firstdate);
+});
